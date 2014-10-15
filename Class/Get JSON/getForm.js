bd6af4607ac8e5e559b69e7163aa83d182c26b01
ttendohv <vhodnett1@student.rcc.edu>
2014-10-15 10:52:36 -0700

    Signed-off-by: ttendohv <vhodnett1@student.rcc.edu>

---
+++ Class/Get JSON/getForm.js
@@ -0,0 +1,17 @@
+// Function which retrieves the information
+// in a form
+function getForm(url){
+	var info=url.split("?");
+	var nameValuePairs=info[1].split("&");
+	var $_GET=new Object;
+	for(var i=0;i<nameValuePairs.length-1;i++){
+		//document.write("<h1>"+nameValuePairs[i]+"</h1>");
+		var map=nameValuePairs[i].split("=");
+		var name=map[0];
+		var value=map[1];
+		$_GET[name]=value;
+	}
+	return $_GET;
+}
+
+var text3 = {"Name":"Victoria","Date":"10%2F15%2F14","Age":"21"};
